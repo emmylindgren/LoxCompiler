325422973f863c24bf3befb89d4e841d1fe46845
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-01-27 14:58:10 +0100

    Worked a bit more on some tokens. String-token parsing started but not sure if done.

--- Scanner.hs
+++ Scanner.hs
@@ -1,8 +1,6 @@
 module Scanner (scanTokens) where
-
 import Data.Maybe
 import Data.Char
-
 import Tokens
 
 --Main function.
@@ -12,33 +10,74 @@ scanTokens x = runTokens x 1
 runTokens :: [Char] -> Int -> [Token]
 runTokens [] _ = []
 runTokens (x:xs) line = case x of 
-    '(' -> addToken LEFT_PAREN [x] line xs
-    ')' -> addToken RIGHT_PAREN [x] line xs
-    '{' -> addToken LEFT_BRACE [x] line xs
-    '}' -> addToken RIGHT_BRACE [x] line xs
-    ',' -> addToken COMMA [x] line xs
-    '.' -> addToken DOT [x] line xs
-    '-' -> addToken MINUS [x] line xs
-    '+' -> addToken PLUS [x] line xs
-    ';' -> addToken SEMICOLON [x] line xs
-    '*' -> addToken STAR [x] line xs
+    '(' -> addSingleToken LEFT_PAREN
+    ')' -> addSingleToken RIGHT_PAREN
+    '{' -> addSingleToken LEFT_BRACE
+    '}' -> addSingleToken RIGHT_BRACE
+    ',' -> addSingleToken COMMA
+    '.' -> addSingleToken DOT
+    '-' -> addSingleToken MINUS
+    '+' -> addSingleToken PLUS
+    ';' -> addSingleToken SEMICOLON
+    '*' -> addSingleToken STAR
     '!' -> if nextMatches '=' xs 
-        then addToken BANG_EQUAL [x] line (tail xs)
-        else addToken BANG [x] line (tail xs)
-    -- Next one to tackle is =, look at the java file. 
-    _  -> [] --Some kind of error handling here
+        then addDoubleToken BANG_EQUAL
+        else addSingleToken BANG
+    '=' -> if nextMatches '=' xs
+        then addDoubleToken EQUAL_EQUAL
+        else addSingleToken EQUAL
+    '<' -> if nextMatches '=' xs
+        then addDoubleToken LESS_EQUAL
+        else addSingleToken LESS
+    '>' -> if nextMatches '=' xs 
+        then addDoubleToken GREATER_EQUAL
+        else addSingleToken GREATER 
+    '/' -> if nextMatches '/' xs 
+        then comment xs line
+        else addSingleToken SLASH
+    ' ' -> noToken 
+    '\r'-> noToken
+    '\t'-> noToken 
+    '\n'-> runTokens xs (line + 1)
+    -- Bryta ut strängskapandet kanske? Känns också som en ganska ful lösning? Snyggare? 
+    -- KOLLA GENOM DETTA ORDENTLIGT! 
+    '"'-> TOKEN STRING "" (STR (fst stringAndRest)) line : runTokens (snd stringAndRest) line
+        where stringAndRest = string xs line
+    -- next one is: isDigit :) look at the java file. Default case? 
+    _  -> error "fel"
+    where 
+        addSingleToken x = addToken x line xs
+        addDoubleToken x = addToken x line (tail xs)
+        noToken = runTokens xs line
 
 
 nextMatches:: Char -> [Char] -> Bool
 nextMatches c [] = False
 nextMatches c (x:xs) = c == x
 
---Denna används ej nu! Kanske behövs sen? 
-createToken:: TokenType -> [Char] -> Int -> Token
-createToken t content = TOKEN t content NONE 
+-- Comments are not added to tokens, it's not meaningfull for 
+-- the parser later on. 
+comment:: [Char] -> Int -> [Token]
+comment [] line = runTokens [] line
+comment (x:xs) line = if x == '\n'
+    then runTokens xs (line + 1)
+    else comment xs line
 
-addToken :: TokenType -> [Char] -> Int -> [Char] -> [Token]
-addToken t content line rest = TOKEN t content NONE line : runTokens rest line 
+-- Problem? Inte så snyggt? Kanske bryta upp det i två funktioner annars?
+string:: [Char] -> Int -> ([Char],[Char])
+string [] line = error ("Unterminated string, at line: "++ show line)
+string (x:xs) line = if x == '"'
+    then ([],xs)
+    else (x : fst stringAndRest, snd stringAndRest)
+    where stringAndRest = string xs line
+
+-- Eller så: 
+-- så länge vi ej stöter på " så returnerar vi den strängen + nästa omgång m xs.
+-- 
+
+addToken :: TokenType -> Int -> [Char] -> [Token]
+addToken t line rest = TOKEN t "" NONE line : runTokens rest line 
+
 --TOKEN tokentype [x] NONE line
 -- 1: Gå genom character för char och utvärdera...
 -- ska jag använda fold?
