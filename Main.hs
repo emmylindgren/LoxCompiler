5f60dd3d917a08da5f735ae04a870ea13c951ca7
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-01 15:55:22 +0100

    Added some more for testing.

--- Main.hs
+++ Main.hs
@@ -2,12 +2,14 @@ module Main (main) where
 import Scanner (scanTokens)
 
 main :: IO ()
+{-
 main =
     let filePath = "C:\\Users\\Emmy\\Documents\\Programspråk\\OU1\\loxsource.lox" in
         readFile filePath >>= \s -> printTokens s
-{-
-main :: IO ()
-main = printTokens "12"-}
+-}
+main =
+    let filePath = "C:\\Users\\Emmy\\Documents\\Programspråk\\OU1\\anotherloxsource.lox" in
+        readFile filePath >>= \s -> printTokens s
 
 printTokens :: String -> IO ()
 printTokens s = mapM_ print $ scanTokens s
