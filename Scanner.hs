8e532946e5342acc6370cce5f3da8c7afd945121
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-01 12:05:37 +0100

    I think all functions are done. Some clean up too. Testing and commenting left.

--- Scanner.hs
+++ Scanner.hs
@@ -39,15 +39,11 @@ runTokens (x:xs) line = case x of
     '\r'-> noToken
     '\t'-> noToken 
     '\n'-> runTokens xs (line + 1)
-    '"'-> let (subString,restString,lineCount) = string xs line
-          in 
-            TOKEN STRING "" (STR subString) line : runTokens restString lineCount
-    -- next one is: isDigit :) look at the java file. Default case? 
+    '"'-> string xs line
     x 
-        | isDigit x -> let (num,restString) = number (x:xs)
-                in
-                TOKEN NUMBER "" (NUM (read num :: Float)) line : runTokens restString line
-        | otherwise -> error "fel"
+        | isDigit x -> number (x:xs) line
+        | isAlpha x || x == '_'-> identifier (x:xs) line
+        | otherwise -> error ("Unexpected value: "++ show x ++" at line "++ show line)
     where 
         addSingleToken x = addToken x line xs
         addDoubleToken x = addToken x line (tail xs)
@@ -70,33 +66,82 @@ comment (x:xs) line = if x == '\n'
     then runTokens xs (line + 1)
     else comment xs line
 
--- Function for collecting strings out of a [Char].
--- Returns: triple containing the substring, 
--- the rest of the [Char] to be examined, and the linecount.
-string:: [Char] -> Int -> ([Char],[Char],Int)
-string [] line = error ("Unterminated string, at line: "++ show line)
-string (x:xs) line = case x of 
-    '"' -> ([],xs,line)
-    '\n' -> advance (line+1)
-    _ -> advance line
-    where 
-        advance line = 
-            let (subString,rest,lineCount) = string xs line
-                in (x:subString,rest,lineCount) 
 
--- Function for collecting a Float out of a [Char].
--- Returns: pair containing substring with the Float, 
--- and the rest of the [Char] to be examined.
-number :: [Char] -> ([Char],[Char])
-number [] = ([],[])
-number (x:xs) = case x of 
-    '.' -> case xs of 
-        [] -> ([],x:xs)
-        xs -> if isDigit (head xs)
-                then let decimals = takeWhile isDigit xs
-                    in (x : decimals, drop (length decimals) xs)
-                else ([],x:xs)
-    x 
-        | isDigit x -> let (num, rest) = number xs
-                in (x:num,rest)
-        | otherwise -> ([],x:xs)
+-- Function for collecting a string Token from [Char].
+-- Returns a list of Tokens following the string token, 
+-- with the string token appended at the head.
+string :: [Char] -> Int -> [Token]
+string list line = 
+    let (subString,rest,newLine) = getString list line
+        in TOKEN STRING "" (STR subString) line : runTokens rest newLine
+    where 
+        getString:: [Char] -> Int -> ([Char],[Char],Int)
+        getString [] line = error ("Unterminated string, at line: "++ show line)
+        getString (x:xs) line = case x of 
+            '"' -> ([],xs,line)
+            '\n' -> advance (line+1)
+            _ -> advance line
+            where 
+                advance line = 
+                    let (subString,rest,lineCount) = getString xs line
+                        in (x:subString,rest,lineCount) 
+
+-- Function for collecting a Float Token out of a [Char].
+-- Returns a list of Tokens following the float token, 
+-- with the float token appended at the head.
+number :: [Char] -> Int -> [Token]
+number list line = 
+    let (numString,restString) = getNumber list
+        in TOKEN NUMBER "" (NUM (read numString :: Float)) line : runTokens restString line
+        where
+            getNumber :: [Char] -> ([Char],[Char])
+            getNumber [] = ([],[])
+            getNumber (x:xs) = case x of 
+                '.' -> case xs of 
+                    [] -> ([],x:xs)
+                    xs -> if isDigit (head xs)
+                            then let decimals = takeWhile isDigit xs
+                                in (x : decimals, drop (length decimals) xs)
+                            else ([],x:xs)
+                x 
+                    | isDigit x -> let (num, rest) = getNumber xs
+                            in (x:num,rest)
+                    | otherwise -> ([],x:xs)
+
+
+identifier :: [Char] -> Int -> [Token]
+identifier list line = 
+    let (name,rest) = getName list
+        in case getKeyWord name of 
+            IDENTIFIER -> TOKEN IDENTIFIER "" (ID name) line : runTokens rest line
+            FALSE -> TOKEN FALSE "" FALSE_LIT line : runTokens rest line
+            TRUE -> TOKEN TRUE "" TRUE_LIT line : runTokens rest line
+            NIL -> TOKEN NIL "" NIL_LIT line : runTokens rest line
+            x -> TOKEN x "" NONE line : runTokens rest line
+    where
+        getName :: [Char] -> ([Char],[Char]) 
+        getName [] = ([],[])
+        getName (x:xs) 
+            | isAlphaNum x || x == '_' = 
+                let (name,rest) = getName xs
+                    in (x:name,rest)
+            | otherwise = ([],x:xs)
+        getKeyWord :: [Char] -> TokenType
+        getKeyWord x = case x of 
+            "and" -> AND 
+            "class" -> CLASS
+            "else" -> ELSE
+            "false" -> FALSE
+            "for" -> FOR
+            "fun" -> FUN
+            "if" -> IF
+            "nil" -> NIL
+            "or" -> OR
+            "print" -> PRINT
+            "return" -> RETURN
+            "super" -> SUPER
+            "this" -> THIS
+            "true" -> TRUE
+            "var" -> VAR
+            "while" -> WHILE
+            _ -> IDENTIFIER
