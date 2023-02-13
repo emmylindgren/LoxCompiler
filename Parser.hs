89d6540321518519dc675dbd3e10b6202938c3fa
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-13 19:42:53 +0100

    Started working a bit on structure, don't really know how to do this yet.

--- Parser.hs
+++ Parser.hs
@@ -9,11 +9,38 @@ data Expr = Num Int
            | Mult Expr Expr
            deriving (Show)
 -}
+--OBS! ALLA datatyper måste definieras om. Nu bara kladd. 
 data A = Num Int
            | Add A A
            | Sub A A
            | Mult A A
            deriving (Show)
+data Expr
+  = IntLit    Int               -- integer constants, leaves of the expression trees
+  | BinaryApp BinOp   Expr Expr
+  | UnaryApp  UnaryOp Expr
 
+data BinOp
+  = MultOp
+  | AddOp
+  | DivOp
+  | SubOp
+
+data UnaryOp
+  = NegOp
+  | AbsOp
+
+--Påbörjat försökt till parsning enligt boken.
 parse :: [Token] -> A
 parse x = Num 3
+
+expression :: [Token] -> A
+expression x = equality x 
+
+-- The rule equality       → comparison ( ( "!=" | "==" ) comparison )* ; 
+equality :: [Token] -> A
+equality x =  let (expr,rest)= comparison x 
+        in case rest of 
+            (x:xs) -> if x == BANG_EQUAL || EQUAL_EQUAL
+                        then BinOp expr x (comparison x)
+                        else expr
