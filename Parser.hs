f8957781d061120ed014d7e168234d2b82ece088
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-14 16:45:31 +0100

    Some more questions raised.

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,5 @@
 module Parser (parse) where
+
 import Tokens
 
 
@@ -8,8 +9,39 @@ data Expr = Num Int
            | Sub Expr Expr
            | Mult Expr Expr
            deriving (Show)
--}
+
 --OBS! ALLA datatyper måste definieras om. Nu bara kladd. 
+
+data Tree = SumNode Operator Tree Tree
+          | ProdNode Operator Tree Tree
+          | AssignNode String Tree
+          | UnaryNode Operator Tree
+          | NumNode Double
+          | VarNode String
+  deriving Show
+-}
+data Tree = Expression
+  deriving Show
+
+--Literal ska ju kunna va en sträng eller ett nummer I guess 
+data Expression = Literal 
+                | Unary Token Expression
+                | Binary Expression Token Expression
+                | Grouping Expression 
+  deriving Show
+
+
+{- Nedan från kap 6 iaf, de är funktionerna som kommer behövas.
+ De ska också returnera ett träd samt resterande del av tokenlistan. 
+expression :: [Token] -> (Tree, [Token])
+equality :: [Token] -> (Tree, [Token])
+comparison :: [Token] -> (Tree, [Token])
+term       :: [Token] -> (Tree, [Token])
+factor     :: [Token] -> (Tree, [Token])
+unary :: [Token] -> (Tree, [Token])
+primary :: [Token] -> (Tree, [Token])
+-}
+
 data A = Num Int
            | Add A A
            | Sub A A
@@ -31,16 +63,25 @@ data UnaryOp
   | AbsOp
 
 --Påbörjat försökt till parsning enligt boken.
+{-
 parse :: [Token] -> A
-parse x = Num 3
+parse x = Num 3 -}
+parse :: [Token] -> A
+parse x = let (tree,rest) = expression x 
+        in 
+          if null rest 
+            then tree
+            else error "Unexpected leftover tokens" --jobba ut denna mer? ska de bli fel här?  
 
-expression :: [Token] -> A
-expression x = equality x 
+
+expression :: [Token] -> (A,[Token])
+expression = equality  
 
 -- The rule equality       → comparison ( ( "!=" | "==" ) comparison )* ; 
-equality :: [Token] -> A
+equality :: [Token] -> (A,[Token])
 equality x =  let (expr,rest)= comparison x 
         in case rest of 
-            (x:xs) -> if x == BANG_EQUAL || EQUAL_EQUAL
-                        then BinOp expr x (comparison x)
-                        else expr
+            (x:xs) -> if x.TokenType == BANG_EQUAL || EQUAL_EQUAL
+                        then (BinOp expr x (comparison x), xs)--ska denna ersätta expr nu?)
+                        -- plus vi ska väl kalla på equality igen på nåt vis?
+                        else (expr,rest)
