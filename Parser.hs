9ec214448495d448889299055641cfe60e694e7b
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-15 20:32:35 +0100

    Some more work done. Expressions almost done, primary left to do.

--- Parser.hs
+++ Parser.hs
@@ -50,7 +50,50 @@ parse x = let (tree,rest) = expression x
 expression :: [Token] -> (Expression,[Token])
 expression = equality  
 
--- The rule equality       → comparison ( ( "!=" | "==" ) comparison )* ; 
+--OBSOBS! Tänk på när listan är slut, när vi stöter på EOF, hanterar vi de fallet ens? 
+equality :: [Token] -> (Expression,[Token])
+equality x = let (equalityExpr,rest) = comparison x 
+    in binaryCheck rest equalityExpr [BANG_EQUAL,EQUAL_EQUAL] comparison
+
+comparison :: [Token] -> (Expression,[Token])
+comparison x = let (comparisonExpr,rest) = term x
+          in binaryCheck rest comparisonExpr [GREATER,GREATER_EQUAL,LESS,LESS_EQUAL] term
+
+term :: [Token] -> (Expression,[Token])
+term x = let (factorExpr,rest) = factor x 
+    in binaryCheck rest factorExpr [MINUS,PLUS] factor
+
+factor :: [Token] -> (Expression,[Token])
+factor x = let (unaryExpr,rest) = unary x 
+    in binaryCheck rest unaryExpr [SLASH,STAR] unary
+
+-- Gjort denna som en hjälpfunktion men vet inte riktigt. Blev ganska rörig faktiskt.
+-- Applicerat på term och factor. Det bör funka. Men tänk på EOF när tar vi hand om de? 
+binaryCheck ::[Token] -> Expression -> [TokenType] -> ([Token] -> (Expression,[Token])) 
+              -> (Expression,[Token])
+binaryCheck (x:xs) leftExpr tokenMatches exprType =
+  if x `match` tokenMatches
+    then let (rightExpr,rest') = exprType xs
+      in binaryCheck rest' Binary{left = leftExpr, operator = x, right = rightExpr} tokenMatches exprType
+    else (leftExpr,x:xs) 
+
+match :: Token -> [TokenType] -> Bool 
+match t matches = let tokenType = getTokenType t in 
+  tokenType `elem` matches 
+
+getTokenType :: Token -> TokenType
+getTokenType (TOKEN t _ _ _) = t 
+
+unary :: [Token] -> (Expression,[Token])
+unary (x:xs) = if x `match` [BANG,MINUS]
+  then let (unaryExpr,rest) = unary xs 
+    in (Unary{operator = x, right= unaryExpr}, rest)
+  else primary 
+
+--next up: primary
+
+{-
+Gamla equality osv. Ifall bättre så? 
 equality :: [Token] -> (Expression,[Token])
 equality x =  let (expr,rest)= comparison x 
         in equalityCheck rest expr
@@ -78,6 +121,26 @@ comparison x = let (expr,rest) = term x
                 then let (rightExpr,rest') = term xs
                   in comparisonCheck rest' Binary{left = leftExpr, operator = x, right = rightExpr}
                 else (leftExpr,x:xs)
---Next up: Term :) 
-getTokenType :: Token -> TokenType
-getTokenType (TOKEN t _ _ _) = t 
+
+term :: [Token] -> (Expression,[Token])
+term x = let (expr,rest) = factor x 
+    in termCheck rest expr
+    where 
+      termCheck :: [Token] -> Expression -> (Expression,[Token])
+      termCheck (x:xs) leftExpr = let tokenType = getTokenType x in
+        if tokenType == MINUS || tokenType == PLUS 
+          then let (rightExpr,rest') = factor xs
+            in termCheck rest' Binary{left = leftExpr, operator = x, right = rightExpr}
+          else (leftExpr,x:xs) 
+
+factor :: [Token] -> (Expression,[Token])
+factor x = let (expr,rest) = unary x 
+    in factorCheck rest expr
+    where 
+      factorCheck :: [Token] -> Expression -> (Expression,[Token])
+      factorCheck (x:xs) leftExpr = let tokenType = getTokenType x in
+        if tokenType == SLASH || tokenType == STAR 
+          then let (rightExpr,rest') = unary xs
+            in factorCheck rest' Binary{left = leftExpr, operator = x, right = rightExpr}
+          else (leftExpr,x:xs) 
+--}
