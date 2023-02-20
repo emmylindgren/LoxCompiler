f30c3b0a5dc59a9a54726719b8587cadbaf3ddbc
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-02-20 16:55:52 +0100

    Forloop support added. Not very pretty though.

--- Parser.hs
+++ Parser.hs
@@ -1,21 +1,22 @@
 module Parser (parse) where
 
 import Tokens
+import Data.Maybe (isNothing, fromJust)
 {- Måste ju ha nån datatyp A som beskrivet i specen = lista av statements. 
 Typ enligt nedan :) 
 -}
-data Tree = Tree [Declaration] 
+data Tree = Tree [Declaration]
   deriving Show
 --Expression är maybe i init pga den kan va "null"
 data Declaration = VarDec{name::Token,initializer::Maybe Expression}
                   | Statement Statement
           deriving (Show)
 
-data Statement = ExpressionStmt Expression 
+data Statement = ExpressionStmt Expression
                 | IfStmt {condition::Expression, thenBranch::Statement,
                           elseBranch:: Maybe Statement}
                 | PrintStmt Expression
-                | WhileStmt {condition::Expression, body::Statement} 
+                | WhileStmt {condition::Expression, body::Statement}
                 | BlockStmt [Declaration]
         deriving (Show)
 
@@ -40,7 +41,7 @@ parse t@(x:xs) = if x `match` [EOF]
     in declarationStmt : parse rest
 
 declaration ::[Token] -> (Declaration,[Token])
-declaration tokens@(x:xs) = case getTokenType x of 
+declaration tokens@(x:xs) = case getTokenType x of
   VAR -> varDeclaration xs
   _ -> let (stmt,rest) = statement tokens in (Statement stmt,rest)
 
@@ -48,10 +49,10 @@ varDeclaration :: [Token] -> (Declaration,[Token])
 varDeclaration tokens@(x:xs) = if x `match` [IDENTIFIER]
   then let (init,first:rest) = getInitializer xs
     in if first `match` [SEMICOLON]
-      then (VarDec{name = x, initializer = init}, rest) 
-      else loxError "Expect ';' after variable declaration in function varDeclaration" x 
+      then (VarDec{name = x, initializer = init}, rest)
+      else loxError "Expect ';' after variable declaration in function varDeclaration" x
   else loxError "Expect variable name in function varDeclaration" x
-  where 
+  where
     getInitializer :: [Token] -> (Maybe Expression,[Token])
     getInitializer t@(first:rest) = if first `match` [EQUAL]
       then let (expr, rest') = expression rest
@@ -61,12 +62,59 @@ varDeclaration tokens@(x:xs) = if x `match` [IDENTIFIER]
 -- Lite frågetecken här. Tror dock printstmt bara ska få resten av listan, han skriver de i boken iaf 
 -- men tror exprstmt behöver hela? gör så i boken också just nu. 
 statement :: [Token] -> (Statement,[Token])
-statement tokens@(x:xs) = case getTokenType x of 
-  IF -> ifStmt xs 
+statement tokens@(x:xs) = case getTokenType x of
+  FOR -> forStmt xs
+  IF -> ifStmt xs
   PRINT -> printStmt xs
   WHILE -> whileStmt xs
   LEFT_BRACE -> blockStmt xs
   _ -> exprStmt tokens
+
+forStmt:: [Token] -> (Statement,[Token])
+forStmt tokens@(x:xs) = if x `match` [LEFT_PAREN]
+  then let (init,rest) = getInit xs in
+    let (cond,rest') = getCond rest in
+      let (incr,rest'') = getIncr rest' in
+        let(body,rest''') = statement rest'' in
+          (createForLoop body incr cond init,rest''')
+  else loxError "Error in function ForStmt. Expected '(' after 'for'" x
+  where
+    getInit:: [Token] -> (Maybe Declaration,[Token])
+    getInit tokens@(x:xs) = case getTokenType x of
+      SEMICOLON -> (Nothing,tokens)
+      VAR -> let (init,rest) = varDeclaration xs in (Just init, rest)
+      _ -> let (init,rest) = exprStmt xs in (Just (Statement init),rest)
+
+    getCond:: [Token] -> (Maybe Expression,[Token])
+    getCond tokens@(x:xs) = if x `match` [SEMICOLON]
+      then (Nothing,tokens)
+      else let (cond,first:rest) = expression tokens in
+        if first `match` [SEMICOLON]
+          then (Just cond,rest)
+          else loxError "Error in function ForStmt. Expected ';' after loop condition" first
+    getIncr:: [Token] -> (Maybe Expression,[Token])
+    getIncr tokens@(x:xs) = if x `match` [RIGHT_PAREN]
+      then (Nothing,tokens)
+      else let (incr,f:rest) = expression tokens in
+        if f `match` [RIGHT_PAREN]
+          then (Just incr,rest)
+          else loxError "Error in function ForStmt. Expected ')' after for clauses" f
+--body incr cond init 
+createForLoop :: Statement -> Maybe Expression -> Maybe Expression -> Maybe Declaration -> Statement
+createForLoop body incr cond init = checkInit (checkCond (checkIncr body incr) cond) init
+  where 
+    checkIncr:: Statement -> Maybe Expression -> Statement
+    checkIncr b i = if isNothing i
+      then b
+      else BlockStmt [Statement b, Statement (ExpressionStmt (fromJust i))]
+    checkCond :: Statement -> Maybe Expression -> Statement
+    checkCond b c = if isNothing c 
+      then WhileStmt{condition= Literal TRUE_LIT,body=b} 
+      else WhileStmt{condition= fromJust c,body=b}
+    checkInit :: Statement -> Maybe Declaration -> Statement
+    checkInit b i = if isNothing i 
+      then b 
+      else BlockStmt[fromJust i,Statement b]
 
 ifStmt:: [Token] -> (Statement,[Token])
 ifStmt tokens@(x:xs) = let (expr,rest) = getExpr tokens
@@ -76,7 +124,7 @@ ifStmt tokens@(x:xs) = let (expr,rest) = getExpr tokens
   where
     getExpr :: [Token] -> (Expression,[Token])
     getExpr(x:xs) = if x `match` [LEFT_PAREN]
-      then let (expr,first:rest) = expression xs 
+      then let (expr,first:rest) = expression xs
           in if first `match` [RIGHT_PAREN]
             then (expr,rest)
             else loxError "Error in function ifStmt.Expect ')' after if condition" first
@@ -114,15 +162,15 @@ blockStmt token = let (declarations,first:rest) = getDeclarations token
           in if first `match` [RIGHT_BRACE]
             then (BlockStmt declarations,rest)
             else loxError "Error in function BlockStmt. Expected '}' after block" first
-  where 
+  where
     getDeclarations :: [Token] -> ([Declaration],[Token])
-    getDeclarations tokens@(x:xs) = if not (x `match` [RIGHT_BRACE] || x `match` [EOF]) 
-      then let (firstdec,rest') = declaration tokens in 
+    getDeclarations tokens@(x:xs) = if not (x `match` [RIGHT_BRACE] || x `match` [EOF])
+      then let (firstdec,rest') = declaration tokens in
         let(restOfDec,rest'') = getDeclarations rest' in (firstdec:restOfDec,rest'')
       else ([],tokens)
 
 exprStmt :: [Token] -> (Statement,[Token])
-exprStmt x = let (expr,first:rest) = expression x 
+exprStmt x = let (expr,first:rest) = expression x
       in if first `match` [SEMICOLON]
       then (ExpressionStmt expr,rest)
       else loxError "Error in function ExprStmt. Expected ';' after expression on line " first
@@ -145,15 +193,15 @@ assignment t = let (expr,first:rest) = orExpr t
 -- Detta funkar [TOKEN IDENTIFIER "" (ID "Hej") 1,TOKEN EQUAL "" NONE 1,TOKEN NUMBER "" (NUM 3.0) 1,TOKEN SEMICOLON "" NONE 1,TOKEN EOF "" NONE 1]
 -- Tror den funkar? 
 checkifVariable :: Expression -> Bool
-checkifVariable (Variable _) = True 
-checkifVariable _ = False 
+checkifVariable (Variable _) = True
+checkifVariable _ = False
 
 orExpr ::[Token] -> (Expression,[Token])
-orExpr x = let (orExpr,rest) = andExpr x 
+orExpr x = let (orExpr,rest) = andExpr x
             in logicalCheck rest orExpr [OR] andExpr
 
 andExpr ::[Token] -> (Expression,[Token])
-andExpr x = let (orExpr,rest) = equality x 
+andExpr x = let (orExpr,rest) = equality x
             in logicalCheck rest orExpr [AND] equality
 
 equality :: [Token] -> (Expression,[Token])
@@ -229,12 +277,12 @@ primary (x:xs) = case getTokenType x of
       in if first `match` [RIGHT_PAREN]
         then (Grouping expr, rest)
         else loxError "Error in function Primary. Expected ')' after grouping on line " first
-  where 
+  where
     saveTokenLiteral = (Literal (getTokenLiteral x), xs)
 
 -------------- Helper functions -------------- 
 match :: Token -> [TokenType] -> Bool
-match t matches = let tokenType = getTokenType t 
+match t matches = let tokenType = getTokenType t
                     in tokenType `elem` matches
 
 getTokenType :: Token -> TokenType
@@ -250,6 +298,6 @@ getTokenLine (TOKEN _ _ _ l) = l
   Function to throw an error in parsing state, 
   It takes two arguments, one of type [Char] (the string containing error info)
   and one of type 'Token', the token of which threw an error. 
--} 
-loxError :: [Char] -> Token -> error 
+-}
+loxError :: [Char] -> Token -> error
 loxError string tok = error (string ++ " on line "++ show (getTokenLine tok))
