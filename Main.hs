f553aba288cd3bc1fdd8748e9541c4f8a3f0e953
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-03-07 19:30:36 +0100

    Added and tested functionality in main for filename as arg.

--- Main.hs
+++ Main.hs
@@ -1,24 +1,41 @@
 module Main (main) where
-
+    
 import Scanner (scanTokens)
 import Parser(parse)
 import Interpreter(interpret)
-
-main :: IO ()
+import System.IO
+import System.Environment
+import System.IO.Error
+import Control.Exception
 {-
-main =
-    let filePath = "C:\\Users\\Emmy\\Documents\\Programspråk\\OU1\\loxsource.lox" in
-        readFile filePath >>= \s -> printTokens s
+-- The following code is for taking the filename in as args! Works.
+main :: IO ()
+main = interpretLoxFile `catch` errorHandler
+
+interpretLoxFile :: IO ()  
+interpretLoxFile = do 
+        (fileName:_) <- getArgs
+        file <- openFile fileName ReadMode  
+        contents <- hGetContents file  
+        printProgram contents  
+
+errorHandler :: IOError -> IO ()  
+errorHandler e  
+    | isDoesNotExistError e = putStrLn "The file does not exist!"  
+    | otherwise = ioError e
 -}
+
 main =
     let filePath = "C:\\Users\\Emmy\\Documents\\Programspråk\\OU1\\anotherloxsource.lox" in
         readFile filePath >>= \s -> printProgram s
 
+
+printProgram :: String -> IO ()
+printProgram s = mapM_ putStrLn $ interpret $ parse $ scanTokens s
+
+--- Used for testing the previous sections ---
 printTokens :: String -> IO ()
 printTokens s = mapM_ print $ scanTokens s
 
 printDeclarations :: String -> IO ()
 printDeclarations s = print $ parse $ scanTokens s
-
-printProgram :: String -> IO ()
-printProgram s = mapM_ print $ interpret $ parse $ scanTokens s
